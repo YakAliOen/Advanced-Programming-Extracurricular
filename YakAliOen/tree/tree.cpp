63c85c80fbccfd8d1fa136316da8f337865b9b99
Yao <156203035+YakAliOen@users.noreply.github.com>
2026-01-21 17:05:13 +0700

    Update tree.cpp

--- YakAliOen/tree/tree.cpp
+++ YakAliOen/tree/tree.cpp
@@ -1,55 +1,174 @@
 #include <iostream>
+#include <string>
+
 using namespace std;
 
-// strut node
-struct Node{
-    string label; // untuk isi data di tree
-    Node *left, *right, *parent;
+/**
+ * Node structure for binary tree
+ * Represents each node in the organizational hierarchy
+ */
+struct Node
+{
+    string label;
+    Node *left;
+    Node *right;
+    Node *parent;
 };
 
-// penjelasan variable pointer di tree
-Node *root = NULL, *newNode = NULL; // linked list
+// Global pointers for tree management
+Node *root = NULL;
+Node *newNode = NULL;
 
-// function untuk create tree
-void createtree(string label){
-    if (root != NULL){
-        cout << "Data root sudah dibuat !!" <<endl;
-    } else {
+/**
+ * Creates the root node of the tree
+ * @param label The label for the root node
+ */
+void createTree(string label)
+{
+    if (root != NULL)
+    {
+        cout << "Root node already exists!" << endl;
+    }
+    else
+    {
         root = new Node();
-        root -> label = label;
-        root -> left = NULL;
-        root -> right = NULL;
-        root -> parent = NULL;
-        cout << "Node: " << label << " berhasil ditambahkan menjadi root" <<endl;
+        root->label = label;
+        root->left = NULL;
+        root->right = NULL;
+        root->parent = NULL;
+        cout << "Node: " << label << " successfully added as root" << endl;
     }
 }
 
-// insert left
-Node *insertleft(string label, Node *node){
-    if (root == NULL){
-        cout << "Buat tree / root terbelih dahulu !!" <<endl;
+/**
+ * Inserts a node as the left child of the given node
+ * @param label The label for the new node
+ * @param node The parent node
+ * @return Pointer to the newly created node, or NULL if operation fails
+ */
+Node *insertLeft(string label, Node *node)
+{
+    if (root == NULL)
+    {
+        cout << "Please create a tree/root first!" << endl;
         return NULL;
-    } else {
-        // if buat menampilkan data yang sudah ada
-        if (node -> left != NULL){
-            cout << "Node: " << node -> label << " berada pada left" <<endl;
-            return NULL;
-        } else { // else apabila belum ada data
-            newNode = new Node();
-            newNode -> label = label;
-            newNode -> left = NULL;
-            newNode -> right = NULL;
-            newNode -> parent = node;
-            node -> left = newNode; // proses masukkan data ke level 2
-            cout << "\n Node: " << label << " berhasil ditambahkan di bagian left " 
-            << newNode -> parent -> label <<endl;
-            return newNode;
-        }
+    }
+    else if (node->left != NULL)
+    {
+        cout << "Node: " << node->label << " already has a left child" << endl;
+        return NULL;
+    }
+    else
+    {
+        newNode = new Node();
+        newNode->label = label;
+        newNode->left = NULL;
+        newNode->right = NULL;
+        newNode->parent = node;
+        node->left = newNode;
+        cout << "Node: " << label << " successfully added as left child of "
+             << newNode->parent->label << endl;
+        return newNode;
     }
 }
 
-int main(){
-    createtree("Owner");
-    Node *nodeperusahaan;
-    nodeperusahaan = insertleft("Staff", root);
+/**
+ * Inserts a node as the right child of the given node
+ * @param label The label for the new node
+ * @param node The parent node
+ * @return Pointer to the newly created node, or NULL if operation fails
+ */
+Node *insertRight(string label, Node *node)
+{
+    if (root == NULL)
+    {
+        cout << "Please create a tree/root first!" << endl;
+        return NULL;
+    }
+    else if (node->right != NULL)
+    {
+        cout << "Node: " << node->label << " already has a right child" << endl;
+        return NULL;
+    }
+    else
+    {
+        newNode = new Node();
+        newNode->label = label;
+        newNode->left = NULL;
+        newNode->right = NULL;
+        newNode->parent = node;
+        node->right = newNode;
+        cout << "Node: " << label << " successfully added as right child of "
+             << newNode->parent->label << endl;
+        return newNode;
+    }
+}
+
+/**
+ * Updates the label of a given node
+ * @param label The new label for the node
+ * @param node The node to be updated
+ */
+void update(string label, Node *node)
+{
+    if (!root)
+    {
+        cout << "Please create a tree first!" << endl;
+    }
+    else if (!node)
+    {
+        cout << "Node does not exist!" << endl;
+    }
+    else
+    {
+        string temp = node->label;
+        node->label = label;
+        cout << "Node label '" << temp << "' successfully changed to '" << label << "'" << endl;
+    }
+}
+
+/**
+ * Performs preorder traversal of the tree
+ * @param node The current node to process
+ */
+void preorder(Node *node)
+{
+    if (!root)
+    {
+        cout << "Please create a tree first!" << endl;
+    }
+    else if (node != NULL)
+    {
+        cout << node->label << ", ";
+        preorder(node->left);
+        preorder(node->right);
+    }
+}
+
+/**
+ * Main function - demonstrates tree creation and operations
+ */
+int main(void)
+{
+    // Create root node
+    createTree("Owner");
+
+    // Declare node pointers for organizational structure
+    Node *nodeStaff, *nodeAdmin, *nodeSecurity, *nodeTechnician;
+
+    // Build organizational hierarchy
+    nodeStaff = insertLeft("Staff", root);
+    nodeAdmin = insertRight("Admin", root);
+    nodeSecurity = insertRight("Security", nodeAdmin);
+    nodeTechnician = insertLeft("Technician", nodeStaff);
+
+    // Update a node label
+    update("Supervisor", nodeAdmin);
+
+    // Display tree in preorder traversal
+    cout << "\nPreorder Traversal:" << endl;
+    preorder(root);
+    cout << endl;
+
+    return 0;
 }
